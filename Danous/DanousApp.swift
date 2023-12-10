ea675104f03729a2c04d85da4c7fc1e27dfcea7e
sanjeev-rm <sanjeevraghu2050@gmail.com>
2023-12-10 11:44:33 +0530

    Initial Commit

---
+++ Danous/DanousApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanousApp.swift
+//  Danous
+//
+//  Created by Sanjeev RM on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DanousApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
