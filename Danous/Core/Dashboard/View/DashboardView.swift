7d9b123bca709aaba6ffe2f01334c2b76d0d50d2
sanjeev-rm <sanjeevraghu2050@gmail.com>
2024-01-03 20:23:31 +0530

    feat: started implementing the dashboard page

--- Danous/Core/Dashboard/View/DashboardView.swift
+++ Danous/Core/Dashboard/View/DashboardView.swift
@@ -8,13 +8,78 @@
 import SwiftUI
 
 struct DashboardView: View {
+    
+    @State var searchText: String = ""
+    @State var userName: String = "John Doe"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            VStack(spacing: 32) {
+                searchBarAndSettingsButton
+                
+                greetingTitle
+                    .padding(.leading)
+            }
+            .padding([.horizontal, .top])
+            .foregroundStyle(.white)
+            
+            featuresGrid
         }
-        .font(.title.monospaced())
-        .foregroundStyle(.accent)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background {
+            Image(danousImage: .backgroundImage)
+                .resizable()
+                .ignoresSafeArea()
+        }
+    }
+}
+
+extension DashboardView {
+    
+    private var searchBarAndSettingsButton: some View {
+        HStack(spacing: 16) {
+            searchBar
+            settingsButton
+        }
+    }
+    
+    private var searchBar: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            TextField("search number or contact", text: $searchText)
+        }
+        .font(.callout)
+        .foregroundColor(.secondary)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 32)
+                .foregroundStyle(Color(uiColor: .tertiarySystemGroupedBackground))
+        )
+    }
+    
+    private var settingsButton: some View {
+        Button {
+        } label: {
+            Image(systemName: "gearshape.fill")
+                .foregroundStyle(Color(uiColor: .white))
+                .font(.title)
+        }
+    }
+    
+    private var greetingTitle: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("Greetings")
+            Text(userName)
+        }
+        .font(.largeTitle.monospaced().bold())
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var featuresGrid: some View {
+        VStack {
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(uiColor: .systemBackground))
     }
 }
 
