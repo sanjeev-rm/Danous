13fd94ee4d2f9cfe8c014dfe7a45d3049fe3c56d
sanjeev-rm <sanjeevraghu2050@gmail.com>
2024-01-05 13:48:55 +0530

    feat: implemented animation of search bar and and entry to dashboard

---
+++ Danous/Core/Dashboard/View/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Danous
+//
+//  Created by Sanjeev RM on 04/01/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
